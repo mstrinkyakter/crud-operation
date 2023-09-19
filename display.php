685a2a340b090197fff8aa2773f1c526cd89a82b
Mst Rinky Akter <mstrinkya@gmail.com>
2023-09-20 01:26:04 +0600

    crud

---
+++ display.php
@@ -0,0 +1,61 @@
+<?php
+include "server.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        
+        <button class="btn btn-primary my-5"> <a href="index.php" class="text-light text-decoration-none">Add User</a> </button>
+
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl no</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+      <th scope="col">Mobile</th>
+      <th scope="col">Operation</th>
+    </tr>
+  </thead>
+
+<?php
+$sql="SELECT * FROM crud_operation";
+$result=mysqli_query($conn,$sql);
+if($result){
+    $row = mysqli_fetch_assoc($result);
+    echo $row['name'];
+}
+?>
+
+  <!-- <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody> -->
+</table>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+include 'server.php';
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $mobile=$_POST['mobile'];
+
+    $sql="INSERT INTO crud_operation(name,email,password,mobile) VALUE ('$name','$email','$password','$mobile')";
+    $result=mysqli_query($conn,$sql);
+    if(!$result){
+        die (mysqli_error()) ;
+    }
+   
+}
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+   <div class="container my-5">
+   <form class="row g-3" method="post">
+   <div class="col-md-6">
+    <label class="form-label">Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="ogg">
+  </div>
+  
+  <div class="col-6">
+    <label class="form-label">Email</label>
+    <input type="email" class="form-control"  placeholder="Enter your email" name ="email" autocomplete="ogg">
+  </div>
+ 
+  <div class="col-md-6">
+    <label class="form-label">Password</label>
+    <input type="password" class="form-control" placeholder="Enter your password" name="password" autocomplete="ogg">
+  </div>
+  <div class="col-6">
+    <label  class="form-label">Mobile</label>
+    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="ogg">
+  </div>
+ 
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary" name="submit">Sign in</button>
+  </div>
+</form>
+   </div>
+    
+  </body>
+</html>
